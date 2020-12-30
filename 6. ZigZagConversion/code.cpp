8291e8eada593344123241b8f2e9e91147923db5
Duc Nguyen <ngminhduc20111997@gmail.com>
2020-12-29 16:47:15 -0800

    Next 4 problems

---
+++ 6. ZigZagConversion/code.cpp
@@ -0,0 +1,77 @@
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+// of rows like this: (you may want to display this pattern in a fixed font for
+// better legibility) P   A   H   N A P L S I I G Y   I   R And then read line
+// by line: "PAHNAPLSIIGYIR" Write the code that will take a string and make
+// this conversion given a number of rows: string convert(string s, int
+// numRows);
+
+// Example 1:
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+
+// Example 2:
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+
+// Example 3:
+// Input: s = "A", numRows = 1
+// Output: "A"
+
+// Constraints:
+// 1 <= s.length <= 1000
+// s consists of English letters (lower-case and upper-case), ',' and '.'.
+// 1 <= numRows <= 1000
+
+#include <string>
+
+class Solution {
+ public:
+  string convert(string s, int numRows) {
+    if (numRows == 1) {
+      return s;
+    }
+
+    int size = s.size();
+    string result = "";
+    int counter = 0;
+    int differenceDown = 2 * (numRows - 1);
+    int differenceUp = 0;
+
+    for (int counter = 0; counter < numRows; counter++) {
+      int i = counter;
+      bool isDown = true;
+      bool update = true;
+      while (i < size) {
+        if (!update) {
+          update = true;
+        } else {
+          result = result + s[i];
+        }
+        if (isDown) {
+          if (counter != numRows - 1) {
+            i += differenceDown;
+          } else {
+            update = false;
+          }
+          isDown = !isDown;
+        } else {
+          if (counter != 0) {
+            i += differenceUp;
+          } else {
+            update = false;
+          }
+          isDown = !isDown;
+        }
+      }
+      differenceDown -= 2;
+      differenceUp += 2;
+    }
+
+    return result;
+  }
+};
---
+++ 7. ReverseInteger/code.cpp
@@ -0,0 +1,42 @@
+// Given a 32-bit signed integer, reverse digits of an integer.
+// Note:
+// Assume we are dealing with an environment that could only store integers
+// within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of
+// this problem, assume that your function returns 0 when the reversed integer
+// overflows.
+
+// Example 1:
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+// Example 4:
+
+// Input: x = 0
+// Output: 0
+
+// Constraints:
+// -231 <= x <= 231 - 1
+
+class Solution {
+ public:
+  int reverse(int x) {
+    int result = 0;
+    while (x != 0) {
+      int rem = x % 10;
+      x /= 10;
+      if ((result > INT_MAX / 10) || (result == INT_MAX / 10 && rem > 7))
+        return 0;
+      if ((result < INT_MIN / 10) || (result == INT_MIN / 10 && rem < -8))
+        return 0;
+      result = result * 10 + rem;
+    }
+    return result;
+  }
+};
