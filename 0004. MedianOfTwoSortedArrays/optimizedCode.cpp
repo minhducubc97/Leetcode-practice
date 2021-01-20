8c01d69ee9daddc604bb94403b8f9dd4669160a2
Duc Nguyen <ngminhduc20111997@gmail.com>
2021-01-20 15:06:36 -0800

    Revisiting problem 4 and 5

--- 0004. MedianOfTwoSortedArrays/optimizedCode.cpp
+++ 0004. MedianOfTwoSortedArrays/optimizedCode.cpp
@@ -1,28 +1,51 @@
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
  public:
   double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int m = nums1.size(), n = nums2.size(), left = (m + n + 1) / 2,
-        right = (m + n + 2) / 2;
-    return (findKth(nums1, 0, nums2, 0, left) +
-            findKth(nums1, 0, nums2, 0, right)) /
-           2.0;
-  }
-  int findKth(vector<int>& nums1, int i, vector<int>& nums2, int j, int k) {
-    if (i >= nums1.size()) return nums2[j + k - 1];
-    if (j >= nums2.size()) return nums1[i + k - 1];
-    if (k == 1) return min(nums1[i], nums2[j]);
-    int midVal1 =
-        (i + k / 2 - 1 < nums1.size()) ? nums1[i + k / 2 - 1] : INT_MAX;
-    int midVal2 =
-        (j + k / 2 - 1 < nums2.size()) ? nums2[j + k / 2 - 1] : INT_MAX;
-    if (midVal1 < midVal2) {
-      return findKth(nums1, i + k / 2, nums2, j, k - k / 2);
+    if (nums1.size() > nums2.size()) {
+      return findMedianSortedArraysSub(nums2, nums1);
     } else {
-      return findKth(nums1, i, nums2, j + k / 2, k - k / 2);
+      return findMedianSortedArraysSub(nums1, nums2);
     }
+  }
+
+  double findMedianSortedArraysSub(vector<int>& smaller, vector<int>& bigger) {
+    int minRange = 0;
+    int maxRange = smaller.size();
+    int halfLen = (bigger.size() + smaller.size() + 1) / 2;
+    while (minRange <= maxRange) {
+      int i = (minRange + maxRange) / 2;
+      int j = halfLen - i;
+      if (i > minRange && smaller[i - 1] > bigger[j]) {
+        maxRange--;
+      } else if (i < maxRange && bigger[j - 1] > smaller[i]) {
+        minRange++;
+      } else {
+        double maxLeftGroup;
+        double minRightGroup;
+        if (i == 0)
+          maxLeftGroup = bigger[j - 1];
+        else if (j == 0)
+          maxLeftGroup = smaller[i - 1];
+        else
+          maxLeftGroup = max(smaller[i - 1], bigger[j - 1]);
+        if ((bigger.size() + smaller.size()) % 2 == 1) {
+          return maxLeftGroup;
+        }
+
+        if (i == smaller.size())
+          minRightGroup = bigger[j];
+        else if (j == bigger.size())
+          minRightGroup = smaller[i];
+        else
+          minRightGroup = min(smaller[i], bigger[j]);
+        return (maxLeftGroup + minRightGroup) / 2;
+      }
+    }
+
+    return 0.0;
   }
 };
