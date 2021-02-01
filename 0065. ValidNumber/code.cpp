3ded541163127280286afa3fb6479200aba559f0
Duc Nguyen <ngminhduc20111997@gmail.com>
2021-02-01 14:21:03 -0800

    Finished problem 65

--- 0065. ValidNumber/code.cpp
+++ 0065. ValidNumber/code.cpp
@@ -44,16 +44,11 @@ using namespace std;
 class Solution {
  public:
   bool isNumber(string s) {
-    bool num = false, numAfterE = true, dot = false, exp = false, sign = false;
+    bool num = false, numAfterE = true, dot = false, exp = false;
     int n = s.size();
     for (int i = 0; i < n; ++i) {
-      if (s[i] == ' ') {
-        if (i < n - 1 && s[i + 1] != ' ' && (num || dot || exp || sign))
-          return false;
-      } else if (s[i] == '+' || s[i] == '-') {
-        if (i > 0 && s[i - 1] != 'e' && s[i - 1] != 'E' && s[i - 1] != ' ')
-          return false;
-        sign = true;
+      if (s[i] == '+' || s[i] == '-') {
+        if (i > 0 && s[i - 1] != 'e' && s[i - 1] != 'E') return false;
       } else if (s[i] >= '0' && s[i] <= '9') {
         num = true;
         numAfterE = true;
