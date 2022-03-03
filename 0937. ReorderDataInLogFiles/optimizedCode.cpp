67bf80ccf5e25b26cd097f018b04ec569c9f0483
minhducubc97 <ngminhduc20111997@gmail.com>
2022-03-03 10:48:34 -0800

    Finished problem 937

---
+++ 0937. ReorderDataInLogFiles/optimizedCode.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        vector<string> res, digitLogs;
+        vector<vector<string>> data;
+        for (string log : logs) {
+            auto pos = log.find(" ");
+            if (log[pos + 1] >= '0' && log[pos + 1] <= '9') {
+                digitLogs.push_back(log);
+                continue;
+            }
+            data.push_back({log.substr(0, pos), log.substr(pos + 1)});
+        }
+        sort(data.begin(), data.end(), [](vector<string>& a, vector<string>& b) {
+            return a[1] < b[1] || (a[1] == b[1] && a[0] < b[0]);
+        });
+        for (auto &a : data) {
+            res.push_back(a[0] + " " + a[1]);
+        }
+        for (string log : digitLogs) res.push_back(log);
+        return res;
+    }
+};
