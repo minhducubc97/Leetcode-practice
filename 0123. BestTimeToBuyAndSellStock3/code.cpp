cce6a48bc74d18571ea24a9e5f7c0181838ee4c5
minhducubc97 <ngminhduc20111997@gmail.com>
2022-04-14 23:51:05 -0700

    Finished problem 123

--- 0123. BestTimeToBuyAndSellStock3/code.cpp
+++ 0123. BestTimeToBuyAndSellStock3/code.cpp
@@ -28,20 +28,20 @@
 class Solution {
   public:
   int maxProfit(vector<int>& prices) {
-    int t1Cost = INT_MAX, 
-        t2Cost = INT_MAX;
-    int t1Profit = 0,
-        t2Profit = 0;
+    int transaction1Cost = INT_MAX, 
+        transaction2Cost = INT_MAX;
+    int transaction1Profit = 0,
+        transaction2Profit = 0;
 
     for (int price : prices) {
         // the maximum profit if only one transaction is allowed
-        t1Cost = min(t1Cost, price);
-        t1Profit = max(t1Profit, price - t1Cost);
+        transaction1Cost = min(transaction1Cost, price);
+        transaction1Profit = max(transaction1Profit, price - transaction1Cost);
         // re-invest the gained profit in the second transaction
-        t2Cost = min(t2Cost, price - t1Profit);
-        t2Profit = max(t2Profit, price - t2Cost);
+        transaction2Cost = min(transaction2Cost, price - transaction1Profit);
+        transaction2Profit = max(transaction2Profit, price - transaction2Cost);
     }
 
-    return t2Profit;
+    return transaction2Profit;
   }
 };
