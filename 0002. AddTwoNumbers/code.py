8c35e3fd42ccbda4e454c6564e1b7cf743843cee
minhducubc97 <ngminhduc20111997@gmail.com>
2026-03-09 14:06:00 -0700

    python AddTwoNumbers

---
+++ 0002. AddTwoNumbers/code.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        result = ListNode(0)
+        curr = result
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            sum = val1 + val2 + carry
+            carry = sum // 10 # floor division
+
+            curr.next = sum % 10
+            curr = curr.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+        return result.next
